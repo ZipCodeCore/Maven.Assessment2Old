a1900ac621e6511a1507759c06b53b87dc8fa6a8
robertgordon <robertgordon.subscript2@gmail.com>
2019-06-17 09:49:46 -0400

    Inventory tests passing

--- src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
+++ src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
@@ -1,15 +1,23 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Use a map to keep track of inventory in a store
  */
 public class Inventory {
+    Map<String, Integer> inventory;
     /**
      * @param strings list of strings to add / remove / fetch from
      */
     public Inventory(List<String> strings) {
+        inventory = new HashMap<>();
+
+        for(String str : strings){
+            inventory.merge(str, 1, (v1, v2) -> v1 + v2);
+        }
 
     }
 
@@ -17,6 +25,7 @@ public class Inventory {
      * nullary constructor initializes a new list
      */
     public Inventory() {
+        inventory = new HashMap<>();
 
     }
 
@@ -24,6 +33,7 @@ public class Inventory {
      * @param item - increment the number of this item in stock by 1
      */
     public void addItemToInventory(String item) {
+        inventory.merge(item, 1, (v1, v2) -> v1 + v2);
         return;
     }
 
@@ -31,6 +41,7 @@ public class Inventory {
      * @param item - decrement the number of this item in stock by 1
      */
     public void removeItemFromInventory(String item) {
+        inventory.merge(item, -1, (v1, v2) -> (v1 == null || v1 == 0) ? 0 : v1 + v2);
         return;
     }
 
@@ -39,6 +50,10 @@ public class Inventory {
      * @return - return the number of items
      */
     public Integer getItemQuantity(String item) {
-        return null;
+        Integer amount = inventory.get(item);
+        if(amount == null){
+            amount = 0;
+        }
+        return amount;
     }
 }
